d314781b6275826cd7130ea40fd7dae31e7638e4
Ivan Platonov <Platonov20030603@gmail.com>
2021-06-12 22:14:07 +0800

    Basic Window

--- WINAPIProject/main.cpp
+++ WINAPIProject/main.cpp
@@ -1,3 +1,54 @@
 #include <iostream>
+#include <Windows.h>
 
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow)
+{
+	MSG msg{};
+	HWND hWnd{};
+	WNDCLASSEX wc{ sizeof(WNDCLASSEX) };
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"WindowClassMain";
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = nullptr;
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hIconSm = nullptr;
+	wc.lpfnWndProc = [](HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)->LRESULT
+	{
+		switch (uMsg)
+		{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(EXIT_SUCCESS);
+		}
+		return 0;
+		}
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	};
+	wc.lpszMenuName = nullptr;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+
+	if (!RegisterClassEx(&wc))
+	{
+		return EXIT_FAILURE;
+	}
+	hWnd = CreateWindow(wc.lpszClassName, L"Window", WS_OVERLAPPEDWINDOW, 500, 300, 800, 600, nullptr, nullptr, hInstance, nullptr);
+	if (hWnd == INVALID_HANDLE_VALUE)
+	{
+		return EXIT_FAILURE;
+	}
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return static_cast<int>(msg.wParam);
+}
 
